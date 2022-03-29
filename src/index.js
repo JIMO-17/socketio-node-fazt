7e79e331fba07540be05c3ce2aa3b0169129a039
JIMO_17_ <jies1702@gmail.com>
2022-03-28 20:16:03 -0500

    socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import express from 'express'
+import app from "./app";
+import { Server as WebsocketServer } from 'socket.io';
+import http from 'http';
 
-console.log("hello world");
+const server = http.createServer(app);
+const httpServer = server.listen(3000);
+const io = new WebsocketServer(httpServer);
+
+// verify connection to socket.io
+// http://localhost:3000/socket.io/socket.io.js
+
+console.log("server is runnign on port 3000");
